3c9574f243a3637ee2a417a0bcd44381d7338137
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-23 03:07:35 +0700

    Add tests for getFleet

--- src/player.test.js
+++ src/player.test.js
@@ -12,3 +12,15 @@ test("getStatus() will return 'AI' if the player is an AI", () => {
     expect(player.getStatus()).toBe("AI");
 });
 
+test("getFleet() will return all ships the player has", () => {
+    let player = new Player();
+
+    player.generateFleet();
+    let fleet = player.getFleet();
+
+    expect(fleet['Carrier'].getLength()).toBe(5);
+    expect(fleet['Battleship'].getLength()).toBe(4);
+    expect(fleet['Destroyer'].getLength()).toBe(3);
+    expect(fleet['Submarine'].getLength()).toBe(3);
+    expect(fleet['Patrol Boat'].getLength()).toBe(2);
+});
