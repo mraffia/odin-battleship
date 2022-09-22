7ae69602ce1d9bddefcbc3d1017ca78322b85879
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 17:52:01 +0700

    Development setup for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: { 
         index: './src/index.js',
     },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
     plugins: [
       new HtmlWebpackPlugin({
-        title: 'Output Management',
+        title: 'Odin Battleship | mraffia',
       }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+      runtimeChunk: 'single',
     },
     module: {
         rules: [
