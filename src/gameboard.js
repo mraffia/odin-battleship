ce0fb6cef601fa1aa6c15fa3282ba409e4261f78
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 23:20:59 +0700

    Add placeShip function logic for GameBoard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,21 +1,39 @@
 const GameBoard = () => {
     let board = {};
+    const row = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
     const getBoard = () => board;
 
     const generateBoard = () => {
-        let hor = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-
         board = {};
         let coor = "";
         
         for (let i = 0; i < 10; i++) {
             for (let j = 1; j < 11; j++) {
-                coor = hor[i] + (j);
-                board[coor] = "o";
+                coor = row[i] + (j);
+                board[coor] = null;
             }
         }
     }
 
+    const placeShip = (ship, coor, axis) => {
+        let tempCoor = coor;
+        let shipRow = coor.charAt(0);
+        let shipCol = coor.charAt(1);
 
+        for (let i = 0; i < ship.getLength(); i++) {
+            board[tempCoor] = "o";
+            ship.setArea(tempCoor);
+
+            if (axis === "hor") {
+                shipCol = Number(shipCol) + 1;
+                tempCoor = shipRow + shipCol;
+            } else if (axis === "ver") {
+                shipRow = row[row.indexOf(shipRow) + 1];
+                tempCoor = shipRow + shipCol;
+            }
+        }
+    }
+
+    export { getBoard, generateBoard, placeShip }
 }
