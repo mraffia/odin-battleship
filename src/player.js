a56c271de54e29ef346c5a6f61c7f0eba9eccb42
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-23 03:15:12 +0700

    Add logic for getFleet and generateFleet

--- src/player.js
+++ src/player.js
@@ -1,7 +1,29 @@
+import { Ship } from "./ship.js";
+import { GameBoard } from "./gameboard.js";
+
 const Player = (status = "human") => {
+    let fleet = {};
+    let playerBoard = [];
+
     const getStatus = () => status;
-    
-    return { getStatus };
+    const getFleet = () => fleet;
+
+    const generateFleet = () => {
+        fleet = {};
+        let carrier = new Ship(5);
+        let battleship = new Ship(4);
+        let destroyer = new Ship(3);
+        let submarine = new Ship(3);
+        let patrolBoat = new Ship(2);
+
+        fleet["Carrier"] = carrier;
+        fleet["Battleship"] = battleship;
+        fleet["Destroyer"] = destroyer;
+        fleet["Submarine"] = submarine;
+        fleet["Patrol Boat"] = patrolBoat;
+    }
+
+    return { getStatus, getFleet, generateFleet };
 }
 
 export { Player };
