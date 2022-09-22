d1c0be98ceb26b84fd7d916814425edf685f433c
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 21:32:53 +0700

    Add test for isSunk Ship function

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,7 +1,14 @@
 import { Ship } from "./ship.js";
 
-test("hit() takes a number and marks that ship's area as 'hit'", () => {
+test("hit(1) takes index 1 and marks that ship's area as 'hit'", () => {
     const ship = new Ship(2);
     ship.hit(1);
-    expect(ship.area[1] === "x");
+    expect(ship.area[1]).toBe("x");
+});
+
+test("isSunk() determines if a ship is sunk or not", () => {
+    const ship = new Ship(2);
+    ship.hit(0);
+    ship.hit(1);
+    expect(ship.isSunk()).toBe(true);
 });
