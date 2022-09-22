e3c902f23132b6d074e334425d5aaa3d62dbba7b
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 23:45:38 +0700

    Add test for receiveAttack function

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -19,10 +19,25 @@ test("placeShip(ship, 'A1', 'ver') able to place a ship at 'A1' coordinate verti
     const ship = new Ship(2);
 
     gameBoard.generateBoard();
-    gameBoard.placeShip(ship, "A1", "ver");\
+    gameBoard.placeShip(ship, "A1", "ver");
 
     let board = gameBoard.getBoard();
     
     expect(board["A1"]).toBe("o");
     expect(board["B1"]).toBe("o");
+});
+
+test("receiveAttack('A1') will mark 'hit' a ship placed at 'A1'", () => {
+    const gameBoard = new GameBoard();
+    const ship = new Ship(2);
+
+    gameBoard.generateBoard();
+    gameBoard.placeShip(ship, "A1", "hor");
+    gameBoard.receiveAttack("A1");
+
+    let board = gameBoard.getBoard();
+    let shipArea = ship.getArea();
+
+    expect(shipArea["A1"]).toBe("x");
+    expect(board["A1"]).toBe("x");
 });
