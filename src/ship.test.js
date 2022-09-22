062e5760d8c5f9d8be2d572e04543ef0e536c10b
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 22:45:06 +0700

    Add tests for setArea and getLength for Ship

--- src/ship.test.js
+++ src/ship.test.js
@@ -8,7 +8,9 @@ test("hit(1) takes index 1 and marks that ship's area as 'hit'", () => {
     gameBoard.placeShip(ship, "A1", "hor");
     ship.hit("A1");
 
-    expect(ship.area["A1"]).toBe("x");
+    let shipArea = ship.getArea();
+
+    expect(ship.shipArea["A1"]).toBe("x");
 });
 
 test("isSunk() determines if a ship is sunk or not", () => {
@@ -20,4 +22,18 @@ test("isSunk() determines if a ship is sunk or not", () => {
     ship.hit("A2");
 
     expect(ship.isSunk()).toBe(true);
+});
+
+test("setArea('A1') will set coordinate 'A1' as one of the ship's area", () => {
+    const ship = new Ship(2);
+    ship.setArea("A1");
+
+    let shipArea = ship.getArea();
+
+    expect(shipArea["A1"]).toBe("o");
+});
+
+test("getLength() will return 2 as ship with length 2", () => {
+    const ship = new Ship(2);
+    expect(ship.getLength()).toBe(2);
 });
