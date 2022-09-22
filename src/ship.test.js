cb7a0a6c50f86ba48394d50e863f10be3fa110e4
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-22 22:06:34 +0700

    Revise tests for hit and isSunk Ship function

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,14 +1,23 @@
 import { Ship } from "./ship.js";
+import { GameBoard } from "./gameboard";
 
 test("hit(1) takes index 1 and marks that ship's area as 'hit'", () => {
+    const gameBoard = new GameBoard();
     const ship = new Ship(2);
-    ship.hit(1);
-    expect(ship.area[1]).toBe("x");
+
+    gameBoard.placeShip(ship, "A1", "hor");
+    ship.hit("A1");
+
+    expect(ship.area["A1"]).toBe("x");
 });
 
 test("isSunk() determines if a ship is sunk or not", () => {
+    const gameBoard = new GameBoard();
     const ship = new Ship(2);
-    ship.hit(0);
-    ship.hit(1);
+
+    gameBoard.placeShip(ship, "A1", "hor");
+    ship.hit("A1");
+    ship.hit("A2");
+
     expect(ship.isSunk()).toBe(true);
 });
