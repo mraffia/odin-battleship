bbf452d2f589e5ff5be19da34db971f9bdffaae2
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-24 01:21:46 +0700

    Simulate a game with random placements and random attacks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import Player from "./factories/player.js";
 
-const start = () => {
+function game() {
     const playerOne = Player();
     const computer = Player("computer");
 
@@ -14,8 +14,36 @@ const start = () => {
     playerOne.randomPlacements();
     computer.randomPlacements();
 
-    console.log(playerOne.getPlayerBoard().getBoard());
-    console.log(computer.getPlayerBoard().getBoard());
+    let bothAlive = true;
+    let turn = 1;
+    let result;
+
+    console.log("START");
+
+    while(bothAlive) {
+        console.log(turn);
+        turn++;
+
+        let playerAttack = playerOne.randomAttack(computer);
+        let computerAttack = computer.randomAttack(playerOne);
+
+        if (playerAttack[1] === true) {
+            if (computer.getPlayerBoard().areAllSunk() === true) {
+                bothAlive = false;
+                result = "Player wins, computer lost"
+            }
+        }
+
+        if (computerAttack[1] === true) {
+            if (playerOne.getPlayerBoard().areAllSunk() === true) {
+                bothAlive = false;
+                result = "Computer wins, player lost"
+            }
+        }
+    }
+
+    console.log(result);
+    console.log("GAME OVER");
 }
 
-start();
+game();
