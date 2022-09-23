2deb816220743dd495dd11bb80403fd73298a728
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-23 22:45:08 +0700

    Add battlePage and shipPlacementPage files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: { 
+        ship: './src/factories/ship.js',
+        gameboard: './src/factories/gameboard.js',
+        player: './src/factories/player.js',
         index: './src/index.js',
+        shipPlacementPage: './src/shipPlacementPage.js',
+        battlePage: './src/battlePage.js',
     },
     devtool: 'inline-source-map',
     devServer: {
