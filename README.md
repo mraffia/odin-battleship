9f6662b49291f9e208c663b8a02f1d5c621eb7e2
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-09-28 03:49:01 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # odin-battleship
 Battleship project from Javascript section of the Full Stack Javascript Course on The Odin Project
+
+### Project Website
+https://mraffia.github.io/odin-battleship/
+
+### Project Description
+A Battleship game where you need to place five of your ships (or randomize them) and battle against a computer.
+
+### Tools
+HTML, CSS (Flexbox & CSS Grid), Vanilla Javascript, ESLint, and Jest (Unit testing)
--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,46 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: { 
-        index: './src/index.js',
-        shipPlacementPage: './src/shipPlacementPage.js',
-        battlePage: './src/battlePage.js',
-        ship: './src/factories/ship.js',
-        gameboard: './src/factories/gameboard.js',
-        player: './src/factories/player.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Odin Battleship | mraffia',
-      }),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    shipPlacementPage: './src/shipPlacementPage.js',
+    battlePage: './src/battlePage.js',
+    ship: './src/factories/ship.js',
+    gameboard: './src/factories/gameboard.js',
+    player: './src/factories/player.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Odin Battleship | mraffia',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    optimization: {
-      runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-        },
-        ],
-    },
+  },
 };
